523c9af8301c3aa667f9fe396ec62ab78d97b3ae
khaledolemy <khaledolemy@gmail.com>
2024-06-25 05:05:28 +0300

    deletion-resilient

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,2 +1,66 @@
 #!/usr/bin/env python3
-""""""
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """gets indexed items caring for continous stream of data"""
+        assert index is not None
+        assert index >= 0
+        data = self.indexed_dataset()
+        assert index <= list(data.keys())[-1]
+        if not index:
+            starting_point = 0
+        else:
+            starting_point = index
+        target_data = []
+        next_index = None
+        for key, value in data.items():
+            if key >= starting_point and len(target_data) < page_size:
+                target_data.append(value)
+                continue
+            if len(target_data) == page_size:
+                next_index = key
+                break
+        return {
+            "index": index,
+            "data": target_data,
+            "page_size": page_size,
+            "next_index": next_index,
+        }
