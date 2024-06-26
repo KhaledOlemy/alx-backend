80fe2325928790bc79ec5adfb59dc3b1cdfb6dd9
khaledolemy <khaledolemy@gmail.com>
2024-06-26 08:17:06 +0300

    fix BasicCache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,8 +7,10 @@ class BasicCache(BaseCaching):
     """Basic Caching System"""
     def put(self, key, item):
         """Puts key:value pair in cache"""
-        self.cache_data[key] = item
+        if key and item:
+            self.cache_data[key] = item
 
     def get(self, key):
         """Gets value from cache using key"""
-        return self.cache_data[key]
+        if key and self.cache_data.get(key):
+            return self.cache_data[key]
