aad8d5137c1a52e679d99d2bc678aa187f5eaf29
khaledolemy <khaledolemy@gmail.com>
2024-07-02 14:19:00 +0300

    app with babel + simple HTML

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Simple WSGI, a basic flask app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Config class/object to config our flask app from"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def homepage():
+    """basic homepage function that renders basic html"""
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
